e74918216507a560a8e53f96411ecf4e52ccd6e9
Abhishek-B-R <byteblaster1468@gmail.com>
2025-07-10 11:02:36 +0530

    updated backend urls

--- core/hooks/use-build-logs.tsx
+++ core/hooks/use-build-logs.tsx
@@ -18,8 +18,8 @@ interface LogEntry {
 
 export function useBuildLogs({
   projectId,
-  wsUrl = "ws://localhost:8082",
-  // wsUrl = "wss://ws.deployr.abhi.wtf",
+  // wsUrl = "ws://localhost:8082",
+  wsUrl = "wss://ws.deployr.abhi.wtf",
   active = true,
   onLogsComplete,
   onReceiveMessage,
