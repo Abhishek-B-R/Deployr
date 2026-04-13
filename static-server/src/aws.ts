2fd219cfb407707c8a30bc06eb79753b04841373
Abhishek B R <byteblaster1468@gmail.com>
2026-04-13 21:05:08 +0530

    trigger backend build

--- static-server/src/aws.ts
+++ static-server/src/aws.ts
@@ -1,23 +1,19 @@
 import { S3 } from "@aws-sdk/client-s3";
-import {
-  R2_ENDPOINT,
-  R2_ACCESS_KEY_ID,
-  R2_SECRET_ACCESS_KEY,
-} from "./envVars";
+import { R2_ENDPOINT, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY } from "./envVars";
 
 const s3 = new S3({
   endpoint: R2_ENDPOINT,
   region: "auto",
   credentials: {
     accessKeyId: R2_ACCESS_KEY_ID,
-    secretAccessKey: R2_SECRET_ACCESS_KEY
-  }
+    secretAccessKey: R2_SECRET_ACCESS_KEY,
+  },
 });
 
 export async function getS3Object(key: string) {
-    const s3Obj = await s3.getObject({
-        Bucket: "deployr-bucket",
-        Key: key
-    });
-    return s3Obj;
+  const s3Obj = await s3.getObject({
+    Bucket: "deployr-bucket",
+    Key: key,
+  });
+  return s3Obj;
 }
